eb68ba162cb2d530e5ee4e6cfa530ed784bb8f6e
Araluka <natnichalnw14@gmail.com>
2024-08-11 10:26:58 +0700

    first commit

--- db_connect.php
+++ db_connect.php
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // ตรวจสอบการเชื่อมต่อ
 if ($conn->connect_error) {
     die("การเชื่อมต่อล้มเหลว: " . $conn->connect_error);
+// http://192.168.2.128/SystemA/Home.php
 }
 ?>
+
